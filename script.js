ab00c2964f87b799e4258b36fe925194cd886839
Marcin Mennemann <marcin@mennemann.net>
2026-05-19 09:43:18 +0200

    formatting

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-const popup = document.getElementById("popup")
-
-
+const popup = document.getElementById("popup");
 
 function newTab(url) {
-    window.open(url, '_blank').focus();
+    window.open(url, "_blank").focus();
 }
 
 function show_mail() {
-    popup.style.display = "block"
+    popup.style.display = "block";
 }
 
 function close_popup() {
-    popup.style.display = "none"
+    popup.style.display = "none";
 }
 
 function copy_input_field(id) {
-    el = document.getElementById(id)
+    el = document.getElementById(id);
     el.select();
     el.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(el.value)
+    navigator.clipboard.writeText(el.value);
 }
 
-
 function scroll_to_el(id, offset) {
-    el = document.getElementById(id)
+    el = document.getElementById(id);
     window.scrollTo({
-        behavior: 'smooth',
-        top:
-            el.getBoundingClientRect().top -
-            document.body.getBoundingClientRect().top -
-            offset,
-    })
+        behavior: "smooth",
+        top: el.getBoundingClientRect().top - document.body.getBoundingClientRect().top - offset,
+    });
 }
